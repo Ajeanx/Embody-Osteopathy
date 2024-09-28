57b2e78ae4a283badf335e794ebca2dea8c046ff
Ertersy40 <wonk4040@gmail.com>
2024-09-28 19:21:43 +1000

    FIXED INVISIBLE NAV BUG

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,17 +1,22 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
+        // FIXME: Animation not working?
+        console.log(nav.style.display)
+        if (nav.style.display === 'flex') {
+            setTimeout(() => {
+                nav.style.display = 'none'
+            }, "500");
+            nav.classList.toggle('nav-active');
+            burger.classList.toggle('toggle');
+        } else {
+            setTimeout(() => {
+                nav.classList.toggle('nav-active');
+                burger.classList.toggle('toggle');
+            }, "100");
+            nav.style.display = 'flex'
+        }
     });
 }
 navSlide();
